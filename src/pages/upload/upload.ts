327c32037b7cba453cee945b8685ee1aa63e1e77
mavl <mvaronamac@gmail.coml>
2018-03-30 13:41:34 +0200

    Initial commit

---
+++ src/pages/upload/upload.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-upload',
+  templateUrl: 'upload.html',
+})
+export class UploadPage {
+
+  constructor() {
+  }
+
+}
